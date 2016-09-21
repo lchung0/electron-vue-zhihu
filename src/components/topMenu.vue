7a8c5cecf1e9d7baab5e32272a1ba37df91bdf8e
Li-Alex <lzhong@coremail.cn>
2016-09-21 15:52:44 +0800

    菜单图标更改，滚动条样式更改

--- src/components/topMenu.vue
+++ src/components/topMenu.vue
@@ -1,8 +1,15 @@
 <template>
-	<a type="button" @click="showMenu">菜单</a>
+	<i class="menu-icon" alt="" @click="showMenu" alt="菜单"></i>
 </template>
 <style lang="less" scoped>
-	
+	.menu-icon{
+		display: inline-block;
+		width: 30px;
+		height: 30px;
+		background-image: url('../assets/icon/menu.svg');
+		background-size: cover;
+		transition: all .5s;
+	}
 </style>
 <script lang="babel">
 	export default{
@@ -14,6 +21,8 @@
 	        },
 			showMenu(){
                 this.isShowMenu = !this.isShowMenu
+                var toggleSrc = !this.isShowMenu ? 'static/images/menu.svg' : 'static/images/close.svg'
+                $('.menu-icon').css('background-image','url(' + toggleSrc + ')') 
                 this.$root.$broadcast('showSideMenu',this.isShowMenu)
             }
 		}
